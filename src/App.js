a56013648f97afe925ceeac6647282802862295d
Shadow-Hokage <yashyadav942004@gmail.com>
2022-05-22 19:00:10 +0530

    Working on feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from "./Feed.js"
 
 function App() {
   return (
---
+++ src/StoryReel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './StoryReel.css'
+
+function StoryReel() {
+  return (
+    <div className='StoryReel'>
+
+    </div>
+  )
+}
+
+export default StoryReel;
